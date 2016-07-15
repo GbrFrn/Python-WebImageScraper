8de7894632d571fe1fbbcef7d8de16ecdcc0c50d
Gabriel Fernandes <gabrielvmfernandes@gmail.com>
2016-07-14 21:00:55 -0300

    Update image_scraper.py

--- image_scraper.py
+++ image_scraper.py
@@ -1,26 +1,52 @@
-from bs4 import BeautifulSoup
-from urllib2 import urlopen
-import urllib
+#Added support to filter by class name of html element
+#Added destination folder support
+#Updated code to work on urllib.request
 
-# use this image scraper from the location that 
-#you want to save scraped images to
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import urllib.request
+
 
 def make_soup(url):
-	html = urlopen(url).read()
-	return BeautifulSoup(html)
+    html = urlopen(url).read()
+    return BeautifulSoup(html, "html.parser")
 
 def get_images(url):
-	soup = make_soup(url)
-	#this makes a list of bs4 element tags
-	images = [img for img in soup.findAll('img')]
-	print (str(len(images)) + "images found.")
-	print 'Downloading images to current working directory.'
-	#compile our unicode list of image links
-	image_links = [each.get('src') for each in images]
-	for each in image_links:
-		filename=each.split('/')[-1]
-		urllib.urlretrieve(each, filename)
-	return image_links
+    soup = make_soup(url)
+    #this makes a list of bs4 element tags
+    images = [img for img in soup.findAll("type of html element", {"class":"name of the class"})]
+    print (str(len(images)) + "images found")
+    print ('Downloading images to directory')
+    #compile our unicode list of image links
+    image_links = [each.get('src') for each in images]
+    for each in image_links:
+        filename=each.split('/')[-1]
+        urllib.request.urlretrieve(each, filename)
+    return image_links
+
+
+#Warning!! If your URL contains "\U" such as \Users it will not work properly
+#To solve this you must double all the \ so that it looks like \\Users
+
+entry = input("Paste the site URL:")
+
+get_images(entry)
+
+import os
+import shutil
+sourcepath="The path where this .py is located at"
+source = os.listdir(sourcepath)
+
+folder = input("Your desired custom path")
+
+destinationpath = folder
+for files in source:
+    if files.endswith('.jpg'):
+        shutil.move(os.path.join(sourcepath,files), os.path.join(destinationpath,files))
+
+
+k=input("Finished! Press any key to exit.")
+
 
 #a standard call looks like this
 #get_images('http://www.wookmark.com')
